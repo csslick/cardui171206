3a340ad926a395c9f954463af065c88e8f129844
csslick <digitone@naver.com>
2017-12-07 01:04:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var card_ui = document.querySelector('.card');
 
 card_ui.click = function(){
+  var href = location.href;
+  console.log(href);
   window.location = 'http://cafe.naver.com/csslick';
 }
